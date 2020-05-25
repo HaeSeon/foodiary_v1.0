95979013bd9b45a128108aaa68a045ddd33402c3
HaeSeon <gildogi@naver.com>
2020-05-26 01:53:19 +0900

    add gallery open

--- app/src/main/java/com/example/foodiary/DiaryEditActivity.kt
+++ app/src/main/java/com/example/foodiary/DiaryEditActivity.kt
@@ -1,9 +1,15 @@
 package com.example.foodiary
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.foodiary.Class.Diary
 import com.example.foodiary.Class.ToEat
 import io.realm.Realm
@@ -11,17 +17,26 @@ import io.realm.kotlin.createObject
 import io.realm.kotlin.where
 import kotlinx.android.synthetic.main.activity_diary_edit.*
 import org.jetbrains.anko.alert
+import org.jetbrains.anko.toast
 import org.jetbrains.anko.yesButton
+import java.lang.Exception
 import java.time.LocalDate
 import kotlin.properties.Delegates
 
 class DiaryEditActivity : AppCompatActivity() {
+
+    private val OPEN_GALLERY = 1;
+
+
+
 
     var realm = Realm.getDefaultInstance()  //인스턴스 얻기
     val d_calendar: LocalDate = LocalDate.now()
 
 
     private var providedToEatId by Delegates.notNull<Long>()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +54,8 @@ class DiaryEditActivity : AppCompatActivity() {
             d_updateMode(d_id)
         }
 
+        //사진 추가 버튼을 클릭하면 갤러리를 연다.
+        button.setOnClickListener { openGallery() }
 
     }
 
@@ -75,6 +92,7 @@ class DiaryEditActivity : AppCompatActivity() {
 
 
     private fun d_insertTodo(){
+
         realm.beginTransaction()    //Realm 에서 데이터를 추가, 삭제 업데이트 할 때는 beginTransaction() 매서드로 트렌젝션 시작.
         //트렌젝션 : 데이터베이스의 작업단위
         //beginTransaction(), commitTransaction() 메서드 사이에 작성한 코드는 전체가 하나의 작업
@@ -84,11 +102,16 @@ class DiaryEditActivity : AppCompatActivity() {
         //새로운  ToEat 타입의 Realm 객체 생성
         val d_newItem = realm.createObject<Diary>(d_nextId())    //createObject() 메서드로 새로운 Realm 객체 생성
 
+
         //값 설정
         d_newItem.d_title = providedToEat.title    //할 일
         d_newItem.d_date = providedToEat.date   //시간 설정, timeInMillis : Long 형 값으로 변환하는 메서드
         d_newItem.d_text = editdiary.text.toString()
-        d_newItem.d_src=editdiary.text.toString()   //음.........뭐받지,,,,,,
+        //d_newItem.d_src=   //음.........뭐받지,,,,,,
+        //toast("${d_newItem.d_src}")
+
+
+
 
         val deleteItem = realm.where<ToEat>().equalTo("id",providedToEatId).findFirst()!!
 
@@ -116,8 +139,10 @@ class DiaryEditActivity : AppCompatActivity() {
 
         //값 설정
         d_update.d_text = editdiary.text.toString()
-        //d_update.d_src=    이미지 바꿔야됨
 
+
+        //d_update.d_src=  //음.........뭐받지,,,,,,
+        //toast("${d_update.d_src}")
         realm.commitTransaction()   //트렌젝션 종료 반영
 
         //다이얼로그 표시
@@ -151,6 +176,38 @@ class DiaryEditActivity : AppCompatActivity() {
         return 0
     }
 
+    //갤러리 열기 함수
+    private fun openGallery(){
+        val intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.setType("image/*")
+        //결과를 수신하기 위하여 startActivity 대신 startActivityForResult 를 호출
+        startActivityForResult(intent,OPEN_GALLERY)
+    }
+
+    //사용자가 startActivityForResult 의 활동을 하고오면  onActivityResult 를 호출
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode== Activity.RESULT_OK){
+
+            if(requestCode==OPEN_GALLERY){
+                //toast("open")
+                var currentImageUrl : Uri? = data?.data
+                try {
+
+                    //Glide 를 통해 이미지를 이미지뷰에 띄워줌
+                    //val bitmap = BitmapFactory.decodeFile(currentImageUrl.toString())
+                    Glide.with(this).load(currentImageUrl).into(imageView)   //이미지를 로딩하고 into()메서드로 imageView 에 표시
+
+                }catch (e: Exception){
+                    e.printStackTrace()
+                }
+            }
+            else{
+                Log.d("ActivityResult", "something wrong")
+            }
+        }
+    }
     override fun onDestroy() {  //액티비티가 소멸되는 생성주기인 onDestroy() 에서 인스턴스 해재
         super.onDestroy()
         realm.close()   //인스턴스 해재
